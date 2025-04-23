abc7d6dc5a6daa7f2692ce0edd95a33c9ee5b737
Mishan Shah <smishan7@gmail.com>
2025-04-23 23:41:24 +0545

    changes in upload logic

--- public/upload.js
+++ public/upload.js
@@ -1,19 +1,34 @@
-const client = new Appwrite.Client()
+import { Client, Storage, Account, ID } from "appwrite";
+
+const client = new Client()
   .setEndpoint("https://syd.cloud.appwrite.io/v1")
   .setProject("6809247f002275dbd4db");
 
-const storage = new Appwrite.Storage(client);
+const storage = new Storage(client);
+const account = new Account(client);
+
+async function ensureAuth() {
+  try {
+    await account.get();
+  } catch (err) {
+    await account.createAnonymousSession();
+  }
+}
 
 export async function uploadTrack(file) {
+  await ensureAuth();
+
   const uploaded = await storage.createFile(
     "680924b2000eaba808f2",
-    "unique()",
+    ID.unique(),
     file
   );
+
   const previewUrl = storage.getFilePreview(
     "680924b2000eaba808f2",
     uploaded.$id
   );
+
   return {
     title: file.name,
     artist: "Unknown",
