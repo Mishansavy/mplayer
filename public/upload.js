4a8a2e34c5c7e293e464d2ca01b8ee708f5f7879
Mishan Shah <smishan7@gmail.com>
2025-04-23 23:45:32 +0545

    fix

--- public/upload.js
+++ public/upload.js
@@ -1,34 +1,19 @@
-import { Client, Storage, Account, ID } from "appwrite";
-
-const client = new Client()
+const client = new Appwrite.Client()
   .setEndpoint("https://syd.cloud.appwrite.io/v1")
   .setProject("6809247f002275dbd4db");
 
-const storage = new Storage(client);
-const account = new Account(client);
-
-async function ensureAuth() {
-  try {
-    await account.get();
-  } catch (err) {
-    await account.createAnonymousSession();
-  }
-}
+const storage = new Appwrite.Storage(client);
 
 export async function uploadTrack(file) {
-  await ensureAuth();
-
   const uploaded = await storage.createFile(
     "680924b2000eaba808f2",
-    ID.unique(),
+    "unique()",
     file
   );
-
   const previewUrl = storage.getFilePreview(
     "680924b2000eaba808f2",
     uploaded.$id
   );
-
   return {
     title: file.name,
     artist: "Unknown",
